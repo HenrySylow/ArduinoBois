d7465d68997d3ce19a6a6079e0f7a8955fdd784d
HenrySylow <48073144+HenrySylow@users.noreply.github.com>
2019-05-02 14:01:43 +1000

    Update Line_Following_Robot.ino

--- Line_Following_Robot.ino
+++ Line_Following_Robot.ino
@@ -14,9 +14,11 @@ float w2;
 float w3;
 float w4;
 float LineDistance;
-int State = 1;
+unsigned int distance_mm;
+int State = 0;
 int StandardSpeed = 80;
 int SpeedDifference = 0;
+int RestSpeed = 0;
 float den;
 float num;
 
@@ -34,35 +36,30 @@ void setup() {
      pinMode(9,OUTPUT); //set IN2 as an output
      pinMode(10,OUTPUT); //set IN3 as an output
      pinMode(11,OUTPUT); //set IN4 as an output
+     pinMode(ECHO, INPUT);
+     pinMode(TRIG, OUTPUT);
      
      Serial.begin(115200);
-     delay(5000);
+     delay(2000);
 }
-void loop() {
-  if(sonar_mm < 100){
-    State = 2;
+void loop() { 
+distance_mm = sonar_mm;
+Serial.print("Distance");
+Serial.println(distance_mm);  
+  if(distance_mm < 100){
+    analogWrite(5,0);
+    analogWrite(6,0);
+    delay(5000);
+    Wall_to_Turn();
   }
-  if( State = 1) {
+  
+  else if( distance_mm>100) {
     LineFollowing();
   }
-  /*else if ( State = 2 ) {
-    WallAssesment();
-    
-  }*/
-}
-
-
-
-/*void WallAssement(){
-  for
-  leftForwards();
-  rightBackwards();
   
-}*/
-
-
-
-
+    
+  
+}
 
 
 void LineFollowing() {
@@ -85,11 +82,11 @@ void LineFollowing() {
           SpeedDifference = StandardSpeed;
      }
     
-     if(LineDistance>-0.5){//right
+     if(LineDistance>-0.5){ //vear right
           analogWrite(6, StandardSpeed + SpeedDifference);
           analogWrite(5, StandardSpeed - SpeedDifference);
      }
-     else if(LineDistance<+0.5){
+     else if(LineDistance<+0.5){ //vear left
           analogWrite(6, StandardSpeed - SpeedDifference);
           analogWrite(5 , StandardSpeed + SpeedDifference);
      }
@@ -100,12 +97,77 @@ void LineFollowing() {
      }
 
 }
+
+void Wall_to_Turn () {
+  
+  analogWrite(5, RestSpeed);
+  analogWrite(6, RestSpeed);
+  delay(1000);
+  leftForwards();
+  rightBackwards();
+  analogWrite(5, 100);
+  analogWrite(6, 100);
+  delay(1300);
+  analogWrite(5, RestSpeed);
+  analogWrite(6, RestSpeed);
+  delay(100);
+  State = 1;
+   
+  
+}
+
+
+
+/*
+ THINGS TO_DO
+
+ SHARP CORNER
+ STOP IN THE SHED
+ CORRIDOR
+ 
+ */
+ void leftForwards(void) {
+    digitalWrite(8,LOW); //IN1
+    digitalWrite(9,HIGH); //IN2
+}
+void leftBackwards(void) {
+    digitalWrite(8,HIGH); //IN1
+    digitalWrite(9,LOW); //IN2
+}
+void rightForwards(void) {
+    digitalWrite(10,LOW); //IN3
+    digitalWrite(11,HIGH); //IN4
+}
+void rightBackwards(void) {
+    digitalWrite(10,HIGH); //IN3
+    digitalWrite(11,LOW); //IN4
+}
+
 unsigned int sonar_mm(void){
 long duration = 0;
-const float speed_sound = 340.29;
+const float speed_sound = 340.29;// m/s, "const" makes the compiler able to optimise
+//... the program where this variable is used, cool!
+// Read in a distance from the ultrasonic distance sensor:
+// The ultrasonic burst is triggered by a HIGH pulse of 10 microseconds.
 digitalWrite(TRIG, HIGH);
 delayMicroseconds(10);
 digitalWrite(TRIG, LOW);
-duration = pulseIn(ECHO, HIGH); 
-return (unsigned int)(0.5 * duration * 1e-6 * speed_sound * 1e3); //Refer to Lab 3
+//read length of time pulse
+duration = pulseIn(ECHO, HIGH); //This function measures a pulsewidth and returns
+//...the width in microseconds
+// convert the time into a distance
+// the code "(unsigned int)" turns the result of the distance calculation
+// into an integer instead of a floating point (decimal or fractional) number.
+return (unsigned int)(0.5 * duration * 1e-6 * speed_sound * 1e3);
+//"unsigned" ensures we are returning an unsigned number, remember that there is no
+//...such thing as negative distance.
 }
+  
+
+ 
+
+
+
+
+
+
